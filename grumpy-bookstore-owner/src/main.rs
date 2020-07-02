6e557cc9a85fe54c7026e97c14859822d7774798
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-07-02 12:14:33 +0300

    Functional.

--- grumpy-bookstore-owner/src/main.rs
+++ grumpy-bookstore-owner/src/main.rs
@@ -1,3 +1,46 @@
+fn max_satisfied(customers: Vec<i32>, grumpy: Vec<i32>, x: i32) -> i32 {
+
+    let definitely_taken : i32 = customers.iter()
+        .enumerate()
+        .fold(0, |sum, (id, val)| {
+            if grumpy[id] == 0 {
+                sum + val
+            } else {
+                sum
+            }
+        });
+
+    let best_gain : i32 = customers
+        .iter()
+        .enumerate()
+        .collect::<Vec::<(usize, &i32)>>()
+        .windows(x as usize)
+        .max_by(|lhs, rhs| {
+            let take_whole_window = |win: &[(usize, &i32)]| {
+                win
+                    .iter()
+                    .fold(0, |sum, (id, val)| {
+                        sum + *val
+                    })
+            };
+
+            let lhsr = take_whole_window(*lhs);
+            let rhsr = take_whole_window(*rhs);
+
+            if std::cmp::max(lhsr, rhsr) == rhsr { std::cmp::Ordering::Less }
+            else { std::cmp::Ordering::Greater }
+        })
+        .unwrap()
+        .iter()
+        .fold(0, |sum, (id, val)| {
+            sum + *val
+        });
+
+    definitely_taken + best_gain
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(
+        3, max_satisfied(vec![1, 2, 3], vec![1, 1, 1], 1)
+    );
 }
