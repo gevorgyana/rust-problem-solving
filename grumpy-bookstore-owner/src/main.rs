ddee71e9941d4165cf0b008a45c0d1a768bb71d3
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-07-02 14:54:52 +0300

    Done

--- grumpy-bookstore-owner/src/main.rs
+++ grumpy-bookstore-owner/src/main.rs
@@ -1,16 +1,8 @@
-static mut debug : bool = false;
-
 fn max_satisfied(customers: Vec<i32>, grumpy: Vec<i32>, x: i32) -> i32 {
     definitely_taken(&customers, &grumpy) + best_gain(&customers, &grumpy, x)
 }
 
 fn definitely_taken(customers: &Vec<i32>, grumpy: &Vec<i32>) -> i32 {
-
-    unsafe {
-        if debug == true {
-
-        }
-    }
 
     customers.iter()
         .enumerate()
@@ -24,45 +16,34 @@ fn definitely_taken(customers: &Vec<i32>, grumpy: &Vec<i32>) -> i32 {
 }
 
 fn best_gain(customers: &Vec<i32>, grumpy: &Vec<i32>, x: i32) -> i32 {
+
+    let gain = |sum : i32, (id, val): &(usize, &i32)| {
+        if grumpy[*id] == 1 {
+            sum + *val
+        } else {
+            sum
+        }
+    };
+
     customers
         .iter()
         .enumerate()
         .collect::<Vec::<(usize, &i32)>>()
         .windows(x as usize)
         .max_by(|lhs, rhs| {
-            let take_whole_window = |win: &[(usize, &i32)]| {
+            let window_gain = |win: &[(usize, &i32)]| {
                 win
                     .iter()
-                    .fold(0, |sum, (id, val)| {
-                        sum + *val
-                    })
+                    .fold(0, gain)
             };
-            let lhsr = take_whole_window(*lhs);
-            let rhsr = take_whole_window(*rhs);
+            let lhsr = window_gain(*lhs);
+            let rhsr = window_gain(*rhs);
             if std::cmp::max(lhsr, rhsr) == rhsr { std::cmp::Ordering::Less }
             else { std::cmp::Ordering::Greater }
         })
         .unwrap()
         .iter()
-        .fold(0, |sum, (id, val)| {
-            sum + *val
-        })
-}
-
-fn bugs() {
-    assert_eq!(
-        definitely_taken(&vec![1, 2, 3], &vec![0, 0, 1]),
-        3
-    );
-
-    assert_eq!(
-        best_gain(&vec![1, 2, 3], &vec![0, 0, 1], 1),
-        2
-    );
-
-    assert_eq!(
-        5, max_satisfied(vec![1, 2, 3], vec![0, 0, 1], 1)
-    );
+        .fold(0, gain)
 }
 
 fn main() {
@@ -78,12 +59,15 @@ fn main() {
         5, max_satisfied(vec![1, 2, 3], vec![1, 0, 1], 1)
     );
     assert_eq!(
-        6, max_satisfied(vec![1, 2, 3], vec![1, 1, 0], 1)
+        5, max_satisfied(vec![1, 2, 3], vec![1, 1, 0], 1)
     );
-
-    /*
     assert_eq!(
-        4, max_satisfied(vec![1, 2, 3], vec![1, 0, 0], 1)
+        6, max_satisfied(vec![1, 2, 3], vec![0, 0, 1], 1)
     );
-     */
+    assert_eq!(
+        6, max_satisfied(vec![1, 2, 3], vec![1, 0, 0], 1)
+    );
+    assert_eq!(
+        6, max_satisfied(vec![1, 2, 3], vec![0, 0, 0], 1)
+    );
 }
