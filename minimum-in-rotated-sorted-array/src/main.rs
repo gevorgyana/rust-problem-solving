f87ab4e5bb585dc28fb887b8a66e8365610389ad
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-07-05 22:26:13 +0300

    Update.

--- minimum-in-rotated-sorted-array/src/main.rs
+++ minimum-in-rotated-sorted-array/src/main.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
 use superslice::*;
+use std::cmp;
 
 fn rotation_index(vec: &Vec<i32>) -> i32 {
     let reversed_vec =
@@ -14,9 +15,9 @@ fn rotation_index(vec: &Vec<i32>) -> i32 {
     println!("{:?}", cooked);
     let compare_tuples = | e : &(&i32, &i32) | {
         if e.0 < e.1 {
-            std::cmp::Ordering::Less
+            cmp::Ordering::Less
         } else {
-            std::cmp::Ordering::Greater
+            cmp::Ordering::Greater
         }
     };
     let result =
@@ -24,33 +25,50 @@ fn rotation_index(vec: &Vec<i32>) -> i32 {
         .lower_bound_by(
             compare_tuples
         );
-
     result as i32
+}
+
+fn minimum_in_rotated_array(rotation_point: i32, vec: &Vec<i32>) -> i32 {
+    if rotation_point == 0 {
+        vec[0]
+    } else {
+        std::cmp::min(vec[rotation_point as usize],
+                  vec[vec.len() - rotation_point as usize])
+    }
 }
 
 fn main() {
     // not moved at all
     assert_eq!(rotation_index(&vec![1, 2, 3, 4]), 0);
+    assert_eq!(minimum_in_rotated_array(0, &vec![1, 2, 3, 4]), 1);
     // moved around the first hole
     assert_eq!(rotation_index(&vec![2, 3, 4, 1]), 1);
+    assert_eq!(minimum_in_rotated_array(1, &vec![2, 3, 4, 1]), 1);
     // moved around the central hole or element, as the index is out of range
     // [0..N / 2 - 1]
     assert_eq!(rotation_index(&vec![3, 4, 1, 2]), 2);
+    assert_eq!(minimum_in_rotated_array(2, &vec![3, 4, 1, 2]), 1);
     // moved around the third hole, which is symmetric with the first hole,
     // the function chooses the lower number, hence 1
     assert_eq!(rotation_index(&vec![4, 1, 2, 3]), 1);
+    assert_eq!(minimum_in_rotated_array(1, &vec![4, 1, 2, 3]), 1);
 
     // not moved at all
     assert_eq!(rotation_index(&vec![1, 2, 3, 4, 5]), 0);
+    assert_eq!(minimum_in_rotated_array(0, &vec![1, 2, 3, 4, 5]), 1);
     // moved aroung the first hole
     assert_eq!(rotation_index(&vec![2, 3, 4, 5, 1]), 1);
+    assert_eq!(minimum_in_rotated_array(1, &vec![2, 3, 4, 5, 1]), 1);
     // moved around the hole that is located to the left from the central
     // element (index out of range)
     assert_eq!(rotation_index(&vec![3, 4, 5, 1, 2]), 2);
+    assert_eq!(minimum_in_rotated_array(2, &vec![3, 4, 5, 1, 2]), 1);
     // moved around the hole that is located to the right from the central
     // element (index out of range)
     assert_eq!(rotation_index(&vec![4, 5, 1, 2, 3]), 2);
+    assert_eq!(minimum_in_rotated_array(2, &vec![4, 5, 1, 2, 3]), 1);
     // symmetric to the prev 1-case: if reversed around the first hole and is of
     // form XY, then current vec is YX, that is symmetric rotation.
     assert_eq!(rotation_index(&vec![5, 1, 2, 3, 4]), 1);
+    assert_eq!(minimum_in_rotated_array(1, &vec![5, 1, 2, 3, 4]), 1);
 }
