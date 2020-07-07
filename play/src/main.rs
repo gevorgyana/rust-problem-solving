7258c781ff9084a6e0fdcc6c3d390d5576dde786
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-07-08 00:39:19 +0300

    Updated the solution to the problem.

--- play/src/main.rs
+++ play/src/main.rs
@@ -18,60 +18,74 @@ fn main() {
 
     let vec = vec!(1, 3, 5);
     assert_eq!(find_min(vec), 1);
+
+    let vec = vec!(2, 2, 2, 0, 1);
+    assert_eq!(find_min(vec), 0);
+
+    let vec = vec!(3, 1, 3);
+    assert_eq!(find_min(vec), 1);
 }
 
 pub fn find_min(nums: Vec<i32>) -> i32 {
     let initial_slice: &[i32] = &nums;
-
     let mut vec: &[i32] = &nums;
     let mut offset : usize = 0;
     let mut pos: usize = 0;
-
+    let mut min: i32 = i32::max_value();
+    println!("Started");
     loop {
-
         if vec.len() < 3 {
             println!("offset is {}", offset);
             if vec.len() == 1 {
                 println!("One");
                 pos = offset;
+                min = std::cmp::min(min, vec[0]);
                 break;
             } else {
                 if vec[0] < vec[1] {
                     println!("Two");
                     pos = offset + 1;
+                    min = std::cmp::min(min, vec[0]);
                     break;
                 } else {
                     println!("Three");
                     pos = offset;
+                    min = std::cmp::min(min, vec[1]);
                     break;
                 }
             }
         };
-
         let m1: usize = vec.len() / 3 - 1;
         let m2: usize = vec.len() - vec.len() / 3;
+        println!("{:?}", vec);
+        println!("Comparing {} to {}", vec[m1], vec[m2]);
+
+        // find min here
+        min = std::cmp::min(min,
+                            std::cmp::min(vec[m1], vec[m2]));
 
         match vec[m1].cmp(&vec[m2]) {
             std::cmp::Ordering::Less => {
                 println!("Less");
                 vec = &vec[m1 + 1..vec.len()];
-                println!("{:?}", vec);
                 offset += m1 + 1;
             },
             std::cmp::Ordering::Equal => {
                 println!("Equal");
                 vec = &vec[m1 + 1..m2];
-                println!("{:?}", vec);
                 offset += m1 + 1;
             },
             std::cmp::Ordering::Greater => {
                 println!("Greater");
                 vec = &vec[0..m2];
-                println!("{:?}", vec);
             },
         }
     }
 
     println!("Survived with pos {}", pos);
-    nums[(pos + 1) % nums.len()]
+
+    std::cmp::min(nums[(pos + 1) % nums.len()],
+                  min)
+
+    // nums[(pos + 1) % nums.len()]
 }
