9a76b6135433868b7ef43f85ab00ab747e797fbd
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-07-16 20:50:22 +0300

    Update. Small notice on how Rust works.

--- play/src/main.rs
+++ play/src/main.rs
@@ -1,10 +1,12 @@
+// how to use mut refs in enumerate()
+
 fn main() {
-    for i in 0..1 {
-        let mut set: std::collections::HashSet<(i32, i32)> =
-            std::collections::HashSet::new();
-        set.insert((1, 1));
-        for j in 0..1 {
-            set.insert((1, 1));
-        }
+    let mut v: Vec<Vec<i32>> = vec![vec![1, 2, 3]];
+    // move is happening here, the value is moved from the vector
+    // doing do will yield an error
+    // for (a, &mut b) in v.iter_mut().enumerate() {
+    // (cannot move borrow mut twice, so we have to move)
+    for (a, mut b) in v.iter_mut().enumerate() {
+        b.reserve(10);
     }
 }
