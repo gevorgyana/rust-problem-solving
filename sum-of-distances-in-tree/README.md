1aa2ca7867c7fc18afe5b326241f5f4a03ec00c9
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-07-25 14:52:40 +0300

    New solution.

---
+++ sum-of-distances-in-tree/README.md
@@ -0,0 +1,7 @@
+The naive solution would look like this: connect the nodes in two
+directions so that we can move from a child to a parent - to be able to
+move from any node in the tree to any other node in it. Then run N BFS
+searches to accumulate the sum of distances to all other nodes on the
+fly. This would take quadratic time.
+
+Here is the proposed solution that would work in linear time.
--- sum-of-distances-in-tree/src/main.rs
+++ sum-of-distances-in-tree/src/main.rs
@@ -1,3 +1,34 @@
+pub fn sum_of_distances_in_tree(n: i32, edges: Vec<Vec<i32>>)
+                                -> Vec<i32> {
+
+    let mut down: Vec<i32>
+        = itertools::repeat_n::<i32>(0, n as usize)
+        .collect::<Vec<i32>>();
+    let mut up: Vec<i32>
+            = itertools::repeat_n::<i32>(0, n as usize)
+        .collect::<Vec<i32>>();
+    let mut graph: Vec<Vec<i32>>
+        = itertools::repeat_n::<Vec<i32>>(vec![], n as usize)
+        .collect::<Vec<_>>();
+
+    for e in &edges {
+        graph[e[0] as usize].push(e[1]);
+    }
+
+    println!("{:?}", graph);
+
+    vec![]
+}
+
 fn main() {
-    println!("Hello, world!");
+    let edges = vec![
+        vec![
+            // always 2 items in an inner array
+            0, 0
+        ],
+        vec![
+            0, 0
+        ]
+    ];
+    sum_of_distances_in_tree(1, edges);
 }
