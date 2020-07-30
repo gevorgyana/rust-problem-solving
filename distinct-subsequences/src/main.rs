6fcf8239ce1051f3ef768410b69f89765cf49b45
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-07-30 14:38:33 +0300

    Dp works for only a case of i >= 2.

--- distinct-subsequences/src/main.rs
+++ distinct-subsequences/src/main.rs
@@ -7,40 +7,52 @@ impl Solution {
      *     +   c
      */
     pub fn num_distinct(s: String, t: String) -> i32 {
-        println!("S: {}", s);
-        println!("T: {}", t);
+        //println!("S: {}", s);
+        //println!("T: {}", t);
 
         let mut dp: Vec<Vec<i32>> = vec![];
-        for i in 0..t.len() {
+        for _i in 0..t.len() - 1 {
             dp.push([0].repeat(s.len()));
         }
         let mut ans: i32 = 0;
 
-        // first row
-        for (i, val) in t.chars().enumerate() {
-            println!("iter t: {}", i);
-            for (j, val2) in s.chars().enumerate() {
-                println!("iter s: {}", j);
-                // small optimization
-                if j < i { continue; }
-                if val2 == val {
+        if s.len() == 0 {
+            return 0;
+        }
 
+        // dp works for length > 1
+        if t.len() == 1 {
+            for (_i, val) in s.chars().enumerate() {
+                if val == t.chars().nth(0).unwrap() {
+                    ans += 1;
+                }
+            }
+            return ans;
+        }
+
+        for (i, val) in t.chars().enumerate() {
+            for (j, val2) in s.chars().enumerate() {
+                if j < i {continue;}
+                if val2 == val {
+                    //println!("t: {}, s: {}", i, j);
                     let mut mul = 1;
                     if i > 0 && dp[i - 1][j] > 0 {
                         mul *= dp[i - 1][j];
                     }
 
-                    if i == t.len() - 1 {
-                        ans += mul;
-                    }
-
-                    for k in j+1..s.len() {
-                        dp[i][k] += mul;
+                    // update the answer or the {dp}
+                    if i == t.len() - 1  && dp[i - 1][j] > 0 {
+                        //println!("adding {}", mul);
+                        ans += dp[i - 1][j];
+                    } else if i < t.len() - 1 {
+                        for k in j+1..s.len() {
+                            dp[i][k] += mul;
+                        }
                     }
                 }
             }
         }
-        // println!("{:?}", dp);
+        //println!("{:?}", dp);
         ans
     }
 }
@@ -68,6 +80,26 @@ mod test {
             String::from("b"),
             String::from("b")),
                    1);
+
+        assert_eq!(Solution::num_distinct(
+            String::from("aabb"),
+            String::from("abb")),
+                   2);
+
+        assert_eq!(Solution::num_distinct(
+            String::from(""),
+            String::from("a")),
+                   0);
+
+        assert_eq!(Solution::num_distinct(
+            String::from("a"),
+            String::from("b")),
+                   0);
+
+        assert_eq!(Solution::num_distinct(
+            String::from("aaa"),
+            String::from("a")),
+                   3);
     }
 }
 
