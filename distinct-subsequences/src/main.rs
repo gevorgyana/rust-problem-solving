e7fb59aa7d1a3acdb3e0147d4e0c2185bb53d3a9
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-07-30 12:38:39 +0300

    Think it is correct.

--- distinct-subsequences/src/main.rs
+++ distinct-subsequences/src/main.rs
@@ -1,3 +1,46 @@
+struct Solution {}
+impl Solution {
+    /*
+     * a|b|c|d    S
+     * +       a  T
+     * --------
+     *     +   c
+     */
+    pub fn num_distinct(s: String, t: String) -> i32 {
+        println!("S: {}", s);
+        println!("T: {}", t);
+
+        let mut dp: Vec<Vec<i32>> = vec![];
+        for i in 0..t.len() {
+            dp.push([0].repeat(s.len()));
+        }
+
+        // first row
+        for (i, val) in t.chars().enumerate() {
+            for (j, val2) in s.chars().enumerate() {
+
+                if val2 == val {
+                    println!("eq s:{} == t:{}", j, i);
+                    println!("from {} to {}", j, s.len() - 1);
+                    let mut mul = 1;
+                    if i > 0 {
+                        mul *= dp[i - 1][j];
+                    }
+                    println!("adding this {}", mul);
+                    for k in j..s.len() {
+                        dp[i][k] += mul;
+                    }
+                }
+            }
+        }
+
+        println!("{:?}", dp);
+
+        1
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let f: i32 = Solution::
+    num_distinct(String::from("abcdac"), String::from("ac"));
 }
--- play/src/main.rs
+++ play/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let l : i32 = (4 as f32).log2().ceil() as i32;
+    let l : i32 = (2 as f32).log2().ceil() as i32;
     println!("{}", l);
 }
