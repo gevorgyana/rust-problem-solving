005726322900dbe9f295bf5dc5a199030c1313e7
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-07-30 11:22:05 +0300

    A way to imiplement log2 on integers.

--- play/src/main.rs
+++ play/src/main.rs
@@ -1,16 +1,4 @@
-// read 2d vector from file into Vec
-extern crate serde;
-
 fn main() {
-    let t: String
-        = std::fs::read_to_string("test").unwrap().parse().unwrap();
-    let v: Result<Vec<Vec<i32>>, _> = serde_json::from_str(
-        &t
-    );
-    match v {
-        Ok(inner) => {
-            println!("Done {:?}", inner);
-        },
-        _ => (),
-    }
+    let l : i32 = (4 as f32).log2().ceil() as i32;
+    println!("{}", l);
 }
