158b43fca1a0d31955a899b6c3c64597dab33c41
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-08-07 23:00:43 +0300

    No need to calculate the rightmost distances.

--- allocate-mailboxes/src/main.rs
+++ allocate-mailboxes/src/main.rs
@@ -2,85 +2,66 @@ struct Solution {}
 impl Solution {
 
     pub fn min_distance(houses: Vec<i32>, k: i32) -> i32 {
-        let base
+
+        println!("{:?}", houses);
+
+        let prefix_by_house
             = Self::min_distance_base(&houses);
 
+        println!("{:?}", prefix_by_house);
+
+        1
+
+        /*
         let mut dp: Vec<Vec<i32>> = vec![];
         for i in 0..=k {
             dp.push(
                 [0].repeat(houses.len() + 1)
             );
         }
+         */
 
-        1
+        /*
+        // first row
+        for i in 1..=houses.len() {
+            dp[1][i] = 1;
+        }
+
+        println!("{:?}", dp);
+         */
+
     }
 
     pub fn min_distance_base(houses: &Vec<i32>) ->
-        Option<Vec<(i32, i32)>> {
-        if houses.len() == 1 {
-            return None;
-        }
-        println!("{:?}", houses);
-        let left_cost = | cost: &mut i32, x: (usize, &[&i32]) | {
-            let diff: i32 = x.1[1] - x.1[0];
-            *cost += diff * (x.0 as i32 + 1);
-            Some(*cost)
-        };
-        let right_cost = | cost: &mut i32, x: (usize, &[&i32]) | {
-            let diff: i32 = x.1[0] - x.1[1];
-            *cost += diff * (x.0 as i32 + 1);
-            Some(*cost)
-        };
-        fn scan_cost<F>(scan_rule: F, vec: Vec<&i32>) -> Vec<i32>
-        where F : FnMut(&mut i32, (usize, &[&i32])) -> Option<i32> {
-            std::iter::once(0)
-                .chain(
-                    vec
-                        .windows(2)
-                        .enumerate()
-                        .scan(0,
-                              scan_rule
-                        )
-                )
-                .collect()
-        };
-        let left_cost: Vec<i32> = scan_cost(
-            left_cost,
-            houses.iter().collect::<Vec<&i32>>()
-        );
-        let right_cost: Vec<i32> = scan_cost(
-            right_cost,
-            houses.iter().rev().collect::<Vec<&i32>>()
-        );
-        println!("{:?}", left_cost);
-        println!("{:?}", right_cost);
-        let packed: Vec<(i32, i32)> = left_cost
-            .into_iter()
-            .zip(
-            right_cost
-                .into_iter()
-                .rev()
-        )
-            .collect();
-        println!("{:?}", packed);
-        Some(packed)
-/*
-        let mut solution: (usize, i32) = (0, i32::max_value());
-        for i in 0..left_cost.len() {
-            let cost = left_cost[i] + right_cost[left_cost.len() - i - 1];
-            println!("!cost = {}", cost);
-            if std::cmp::min(cost, solution.1) == cost {
-                solution.1 = cost;
-                solution.0 = i;
-            }
-        }
-
-        solution.0 as i32
-*/
+        Vec<(usize, i32)>
+    {
+        // at the first house, we have 0 houses to the left and 0 cost of
+        // reaching them from this point.
+        std::iter::once((0, 0))
+            .chain(
+                houses
+                    .iter()
+                    .collect::<Vec<&i32>>()
+                    .windows(2)
+                    .enumerate()
+                // we have started from this value and will go on with
+                // it here in the loop
+                    .scan((0, 0),
+                          |
+                          cost: &mut (usize, i32),
+                          x: (usize, &[&i32])
+                          | {
+                              let diff: i32 = x.1[1] - x.1[0];
+                              cost.1 += diff * (x.0 as i32 + 1);
+                              cost.0 += 1;
+                              Some(*cost)
+                          }
+                    )
+            )
+            .collect()
     }
 }
 
 fn main() {
-
     Solution::min_distance(vec![1, 3, 5], 2);
 }
