a2fda04781cd8340349b6b045d648a3f676a3ec6
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-08-08 12:58:24 +0300

    Remove excessive calculations.

--- allocate-mailboxes/src/main.rs
+++ allocate-mailboxes/src/main.rs
@@ -4,40 +4,31 @@ impl Solution {
     pub fn min_distance(houses: Vec<i32>, k: i32) -> i32 {
 
         println!("{:?}", houses);
-
         let prefix_by_house
             = Self::min_distance_base(&houses);
-
         println!("{:?}", prefix_by_house);
 
-        1
-
-        /*
+        // As usual, {i} is for # of houses from the left that are used
+        // {j} is the amount of resource that we have - in this case
+        // houses
         let mut dp: Vec<Vec<i32>> = vec![];
         for i in 0..=k {
             dp.push(
                 [0].repeat(houses.len() + 1)
             );
         }
-         */
 
-        /*
-        // first row
-        for i in 1..=houses.len() {
-            dp[1][i] = 1;
-        }
 
-        println!("{:?}", dp);
-         */
 
+        1
     }
 
     pub fn min_distance_base(houses: &Vec<i32>) ->
-        Vec<(usize, i32)>
+        Vec<i32>
     {
         // at the first house, we have 0 houses to the left and 0 cost of
         // reaching them from this point.
-        std::iter::once((0, 0))
+        std::iter::once(0)
             .chain(
                 houses
                     .iter()
@@ -46,14 +37,13 @@ impl Solution {
                     .enumerate()
                 // we have started from this value and will go on with
                 // it here in the loop
-                    .scan((0, 0),
+                    .scan(0,
                           |
-                          cost: &mut (usize, i32),
+                          cost: &mut i32,
                           x: (usize, &[&i32])
                           | {
                               let diff: i32 = x.1[1] - x.1[0];
-                              cost.1 += diff * (x.0 as i32 + 1);
-                              cost.0 += 1;
+                              *cost += diff * (x.0 as i32 + 1);
                               Some(*cost)
                           }
                     )
