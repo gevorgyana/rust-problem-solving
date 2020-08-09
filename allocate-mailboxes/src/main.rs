cd06000126b3bbe88e96e04939ab4e60596a2f3a
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-08-09 13:24:18 +0300

    Fix the linear search on houses.

--- allocate-mailboxes/src/main.rs
+++ allocate-mailboxes/src/main.rs
@@ -20,6 +20,7 @@ impl Solution {
 
         println!("{:?}", left_prefix);
 
+        /*
         // calculate for houses their right prices
         // right price is left price with a shift.
         for i in 0..houses.len() {
@@ -29,6 +30,28 @@ impl Solution {
                 - left_prefix[i]
                 - i as i32 * (houses[houses.len() - 1] - houses[i]);
             println!("left {}, right {}", left_cost, right_cost);
+        }
+         */
+
+        // calculate the best answer for each # of houses that are
+        // available. let's say first that we can only put mailboxes
+        // at houses.
+
+        for hm_houses_avail in 1..houses.len() {
+            println!("# houses {}", hm_houses_avail);
+            for checked in 0..hm_houses_avail {
+                println!("checked {}", checked);
+                let left_cost
+                    = left_prefix[checked];
+                let right_cost
+                    = left_prefix[hm_houses_avail - 1]
+                    - left_prefix[checked]
+                    - checked as i32 *
+                    (houses[hm_houses_avail - 1] - houses[checked]);
+                println!("left cost {}; right cost {}",
+                         left_cost,
+                         right_cost);
+            }
         }
 
         1
