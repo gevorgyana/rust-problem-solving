b94fc79a9075441d9bc2f9163cb3a179cf3608da
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-08-09 20:05:38 +0300

    Bug spotted.

--- allocate-mailboxes/src/main.rs
+++ allocate-mailboxes/src/main.rs
@@ -86,109 +86,107 @@ impl Solution {
 
         // println!("{:?}", dp);
 
-        // 2 mailboxes and calculate just the needed answer now -
-        // calculate only for the last one.
-        for left_part_sz in 1..houses.len() {
-            // println!("");
-            // println!("size of the left part {}", left_part_sz);
-            for hm_houses_avail in 1..=houses.len() - left_part_sz {
-                // println!("# houses available {}", hm_houses_avail);
-                let mut ans = i32::max_value();
+        for kc in 2..=k {
+            for left_part_sz in 1..houses.len() {
+                // println!("");
+                // println!("size of the left part {}", left_part_sz);
+                for hm_houses_avail in 1..=houses.len() - left_part_sz {
+                    // println!("# houses available {}", hm_houses_avail);
+                    let mut ans = i32::max_value();
 
-                for window_start in 0..hm_houses_avail - 1 {
+                    for window_start in 0..hm_houses_avail - 1 {
 
-                    let window_start = window_start + left_part_sz;
-                    let hm_houses_avail = hm_houses_avail + left_part_sz;
+                        let window_start = window_start + left_part_sz;
+                        let hm_houses_avail = hm_houses_avail + left_part_sz;
 
-                    for checked in houses[window_start]..
-                        houses[window_start + 1] {
+                        for checked in houses[window_start]..
+                            houses[window_start + 1] {
 
-                            let left_cost
-                                = left_prefix[window_start]
-                                - left_prefix[left_part_sz]
-                                - left_part_sz as i32
-                                * (houses[window_start]
-                                   - houses[left_part_sz]
-                                )
-                                + (checked - houses[window_start]);
+                                let left_cost
+                                    = left_prefix[window_start]
+                                    - left_prefix[left_part_sz]
+                                    - left_part_sz as i32
+                                    * (houses[window_start]
+                                       - houses[left_part_sz]
+                                    )
+                                    + (checked - houses[window_start]);
 
-                            let left_prefix_this_rightmost_window
-                                = left_prefix[window_start + 1]
-                                - left_prefix[left_part_sz]
-                                - left_part_sz as i32
-                                * (houses[window_start + 1]
-                                   - houses[left_part_sz])
-                                ;
+                                let left_prefix_this_rightmost_window
+                                    = left_prefix[window_start + 1]
+                                    - left_prefix[left_part_sz]
+                                    - left_part_sz as i32
+                                    * (houses[window_start + 1]
+                                       - houses[left_part_sz])
+                                    ;
 
-                            let left_prefix_this_last_house
-                                = left_prefix[hm_houses_avail - 1]
-                                - left_prefix[left_part_sz]
-                                - left_part_sz as i32
-                                * (houses[hm_houses_avail - 1]
-                                   - houses[left_part_sz])
-                                ;
+                                let left_prefix_this_last_house
+                                    = left_prefix[hm_houses_avail - 1]
+                                    - left_prefix[left_part_sz]
+                                    - left_part_sz as i32
+                                    * (houses[hm_houses_avail - 1]
+                                       - houses[left_part_sz])
+                                    ;
 
-                            let right_cost_window_1
-                                = left_prefix_this_last_house
-                                - left_prefix_this_rightmost_window
-                                - ((window_start as i32 + 1) - left_part_sz as i32)
-                                * (houses[hm_houses_avail - 1]
-                                   - houses[window_start + 1]);
+                                let right_cost_window_1
+                                    = left_prefix_this_last_house
+                                    - left_prefix_this_rightmost_window
+                                    - ((window_start as i32 + 1) - left_part_sz as i32)
+                                    * (houses[hm_houses_avail - 1]
+                                       - houses[window_start + 1]);
 
-                            let additional_shift
-                                = (hm_houses_avail as i32 - 1
-                                   - window_start as i32)
-                                * (houses[window_start + 1]
-                                   - checked)
-                                ;
+                                let additional_shift
+                                    = (hm_houses_avail as i32 - 1
+                                       - window_start as i32)
+                                    * (houses[window_start + 1]
+                                       - checked)
+                                    ;
 
-                            let right_cost
-                                = right_cost_window_1
-                                + additional_shift
-                                ;
+                                let right_cost
+                                    = right_cost_window_1
+                                    + additional_shift
+                                    ;
 
-                            ans = std::cmp::min(
-                                ans,
-                                left_cost + right_cost
-                            );
+                                ans = std::cmp::min(
+                                    ans,
+                                    left_cost + right_cost
+                                );
 
-                            // println!("???left cost {}", left_cost);
-                            // println!("!!!right cost {}", right_cost);
-                        }
+                                // println!("???left cost {}", left_cost);
+                                // println!("!!!right cost {}", right_cost);
+                            }
+                    }
+
+                    let left_cost
+                        = left_prefix[hm_houses_avail - 1 + left_part_sz]
+                        - left_prefix[left_part_sz]
+                        - (left_part_sz) as i32
+                        * (houses[hm_houses_avail - 1 + left_part_sz]
+                           - houses[left_part_sz]
+                        );
+
+                    let right_cost = 0;
+
+                    /*
+                    println!("the cost of solving k = {}, n = {} is {}",
+                    1,
+                    left_part_sz,
+                    dp[1][left_part_sz]
+                );
+                     */
+
+                    ans = std::cmp::min(
+                        ans,
+                        left_cost + right_cost
+                    );
+
+                    dp[kc as usize][hm_houses_avail + left_part_sz]
+                        = ans
+                        + dp[kc as usize - 1][left_part_sz as usize]
+                        ;
                 }
-
-                let left_cost
-                    = left_prefix[hm_houses_avail - 1 + left_part_sz]
-                    - left_prefix[left_part_sz]
-                    - (left_part_sz) as i32
-                    * (houses[hm_houses_avail - 1 + left_part_sz]
-                       - houses[left_part_sz]
-                       );
-
-                let right_cost = 0;
-
-                /*
-                println!("the cost of solving k = {}, n = {} is {}",
-                         1,
-                         left_part_sz,
-                         dp[1][left_part_sz]
-                );
-                 */
-
-                ans = std::cmp::min(
-                    ans,
-                    left_cost + right_cost
-                );
-
-                dp[2][hm_houses_avail + left_part_sz]
-                    = ans
-                    + dp[1][left_part_sz]
-                    ;
             }
         }
-
-        // println!("{:?}", dp);
-
+        println!("{:?}", dp);
         dp[k as usize][houses.len() as usize]
     }
 
@@ -217,6 +215,14 @@ mod test {
         assert_eq!(
             Solution::min_distance(vec![1, 2, 3, 4, 5], 2),
             4
+        );
+    }
+
+    #[test]
+    fn lc1() {
+        assert_eq!(
+            Solution::min_distance(vec![1,4,8,10,20], 3),
+            5
         );
     }
 }
