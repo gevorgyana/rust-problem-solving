251662920f8d79fd70a3009eefaea149bb216810
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-08-09 15:17:24 +0300

    Update.

--- allocate-mailboxes/src/main.rs
+++ allocate-mailboxes/src/main.rs
@@ -20,25 +20,21 @@ impl Solution {
 
         println!("{:?}", left_prefix);
 
-        /*
-        // calculate for houses their right prices
-        // right price is left price with a shift.
-        for i in 0..houses.len() {
-            let left_cost = left_prefix[i];
-            let right_cost
-                = left_prefix[houses.len() - 1]
-                - left_prefix[i]
-                - i as i32 * (houses[houses.len() - 1] - houses[i]);
-            println!("left {}, right {}", left_cost, right_cost);
+        let mut dp: Vec<Vec<i32>> = vec![];
+        for i in 0..=k {
+            dp.push([0].repeat(houses.len() + 1));
         }
-         */
 
         // calculate the best answer for each # of houses that are
         // available. let's say first that we can only put mailboxes
-        // at houses.
+        // at houses. This is base case of dp.
 
+        // allocate at any point in the range now
         for hm_houses_avail in 1..houses.len() {
             println!("# houses {}", hm_houses_avail);
+            let mut ans = i32::max_value();
+
+            /*
             for checked in 0..hm_houses_avail {
                 println!("checked {}", checked);
                 let left_cost
@@ -51,8 +47,101 @@ impl Solution {
                 println!("left cost {}; right cost {}",
                          left_cost,
                          right_cost);
+
+                ans = std::cmp::min(
+                    ans,
+                    (left_cost - right_cost).abs()
+                    );
             }
+             */
+
+            for window_start in 0..hm_houses_avail - 1 {
+                println!("window start {}, end {}",
+                         window_start, window_start + 1);
+                println!("house start {}, end {}",
+                         houses[window_start],
+                         houses[window_start + 1]);
+
+                for checked in houses[window_start]..
+                    houses[window_start + 1] {
+
+                        let left_cost
+                            = left_prefix[window_start]
+                            + (checked - houses[window_start]);
+
+                        println!(" ans for the last node {} - and for
+the current rightmost node in the window {} - # nodes to the left from the rightmost window node {} * distance from the rightmost window node to the rightmost node {} + hm nodes the right from the tested point {} * the distance from the tested point and the rightmost window node {}",
+
+                                 left_prefix[hm_houses_avail - 1],
+                                 left_prefix[window_start + 1],
+                                 window_start + 1,
+                                 houses[hm_houses_avail - 1]
+                                 - houses[window_start + 1],
+                                 hm_houses_avail - 1
+                                 - window_start,
+                                 houses[window_start + 1]
+                                 - checked
+
+                        );
+
+                        let right_cost
+                            =
+                            left_prefix[hm_houses_avail - 1]
+                            -
+                            left_prefix[window_start + 1]
+                            -
+                            (window_start as i32 + 1)
+                            *
+                            (houses[hm_houses_avail - 1]
+                             - houses[window_start + 1])
+                            +
+                            (hm_houses_avail as i32 - 1
+                             - window_start as i32)
+                            *
+                            (houses[window_start + 1]
+                             - checked)
+                            ;
+
+                        ans = std::cmp::min(
+                            ans,
+                            (left_cost - right_cost).abs()
+                        );
+
+                        println!("left cost {}", left_cost);
+                        println!("right cost {}", right_cost);
+                    }
+            }
+
+            // check the last house - this works when N = 1
+            println!("checking the last house, its index is {}",
+                     hm_houses_avail - 1
+            );
+            println!("coordinate of the last hosue {}",
+                     houses[hm_houses_avail - 1]);
+            let left_cost
+                = left_prefix[hm_houses_avail - 1];
+            let right_cost = 0;
+            println!("left cost {}", left_cost);
+            println!("right cost {}", right_cost);
+
+            ans = std::cmp::min(
+                ans,
+                (left_cost - right_cost).abs()
+            );
+            println!("?The answer {}", ans);
+            dp[1][hm_houses_avail] = ans;
         }
+
+        println!("{:?}", dp);
+
+        /*
+        // for 2 mailboxes...
+        for hm_houses_avail in 2..houses.len() {
+            // go on . do not use optimizations for now. use linear search
+            // to pick the best answer
+
+        }
+         */
 
         1
     }
@@ -75,5 +164,5 @@ impl Solution {
 }
 
 fn main() {
-    Solution::min_distance(vec![1, 2, 3, 4, 5], 1);
+    Solution::min_distance(vec![1, 2, 3, 4, 5], 2);
 }
